502dacd585e5c00a4440af177a70f0161bb77797
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2026-03-15 10:40:40 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.31.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "jenkins_ec2" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Jenkins-Terraform-Instance"
+  }
+}
